1d0391472f4ac565c83a52a14ce0cef5e2e8b3cd
MaureenFromuth <68035458+MaureenFromuth@users.noreply.github.com>
2021-01-10 16:12:13 -0500

    Add files via upload

---
+++ Visualization/static/js/change-distribution.js
@@ -0,0 +1,17 @@
+function age(){ 
+  document.getElementById('myImage') 
+  .src="static/images/Age-Cat_Distribution.png"; 
+  document.getElementById('title') 
+  .innerHTML="Age Distribution per JobCategory"; 
+  document.getElementById('description') 
+  .innerHTML="This is test words 1"; 
+} 
+
+function yearsrole(){ 
+  document.getElementById('myImage') 
+  .src="static/images/YrsInCurrRole vs Cat-Distribution.png"; 
+  document.getElementById('title') 
+  .innerHTML="Years In Current Role Distribution per JobCategory"; 
+  document.getElementById('description') 
+  .innerHTML="This is test words 2"; 
+} 
